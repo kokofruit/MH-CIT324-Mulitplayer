de8bac33d3b0ddf070e37665044090906df632a3
Moth Harper <103866827+kokofruit@users.noreply.github.com>
2025-11-17 21:26:28 -0500

    Steps 15-18

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,8 @@
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
     public float moveSpeed = 5f;
     
@@ -9,11 +10,16 @@ public class PlayerController : MonoBehaviour
 
     public void OnMove(InputValue inputValue)
     {
+        // capture movement
         movementVector = inputValue.Get<Vector2>();
     }
 
     private void Update()
     {
+        // allow only the owner to facilitate updates
+        if (!IsOwner) return;
+
+        // apply movement
         Vector3 movementDirecion = new Vector3(movementVector.x, movementVector.y, 0);
         transform.Translate(movementDirecion * moveSpeed * Time.deltaTime);
     }
